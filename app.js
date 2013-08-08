6ee30651a434adb32f373a88147676b4a87bf1f9
Florian Harbecke <florian@harbecke.me>
2013-08-08 16:03:13 +0200

    second commit

--- app.js
+++ app.js
@@ -7,12 +7,20 @@
 			// next set the objects to a new Array
 			// if its a DOMnode, add it to the Objects Array
 			// if no context is defined, set context to document
-			// if its an id, use getElementById because its faster (using context beacuse its shorter when compressed)
-			// if its not an id, use querySelectorAll to get classes etc.
+			// use querySelectorAll to get classes, ids etc.
 			// if this is true, return the _$ Object else it is a function so return the _$.ready(selector)
-			selector && (_$.o = []);
-			!context && (context = document);
-			return !_$.isfn(selector) && (!selector || selector.nodeType && _$.o.push(selector) || /^#/.test(selector) && _$.o.push(context.getElementById(selector)) || (_$.o = context.querySelectorAll(selector))) ? _$ : _$.ready(selector);
+			return !_$.isfn(selector) && (selector.nodeType && (_$.o = []).push(selector) || (_$.o = (!context ? document : context).querySelectorAll(selector))) ? _$ : _$.ready(selector);
+		},
+		ready: function(fn) {
+			dcl = 'DOMContentLoaded';
+			fire = function() {
+				// unbind the listeners
+				!$(document).unbind(dcl, fire) && (window.onload = null);
+				// using call like jquery
+				fn.call(_$, $);
+			};
+			// if its not available use window.onload
+			!$(document).bind(dcl, fire) && (window.onload = fire);
 		},
 		isfn: function(fn) {
 			// not jquery like, but works ;)
@@ -20,7 +28,7 @@
 		},
 		each: function(fn) {
 			// adapted from jquery
-			for (i = 0; i < _$.o.length; i++) { fn.call(_$.o[i]); }
+			for (i = 0; i < _$.o.length; i++) { fn.call(_$.o[i], i); }
 		},
 		attr: function(name, val) {
 			_$.each(function() {
@@ -36,21 +44,10 @@
 			});
 		},
 		unbind: function(e, fn) {
-			_$.each(function(o) {
+			_$.each(function() {
 				// test if its available else return false / 0 (to use window.onload...)
 				return ((o = this).rm = o.removeEventListener) && o.rm(e, fn, 0) ? _$ : 0;
 			});
-		},
-		ready: function(fn) {
-			dcl = 'DOMContentLoaded';
-			fire = function(e) {
-				// unbind the listeners
-				!$(document).unbind(dcl, fire) && (window.onload = null);
-				// not using call...
-				fn.call(_$, $);
-			};
-			// if its not available use window.onload
-			!$(document).bind(dcl, fire) && (window.onload = fire);
 		}
 	};
 	// haha too simple...
