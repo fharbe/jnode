75bee0fa18208b53e7f2b7e93bbf802f15d4402e
Florian Harbecke <florian@harbecke.me>
2013-08-08 16:04:12 +0200

    fifth commit

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-$ = function(selector, context) {
+$ = function(cb) {
+	var dcl = 'DOMContentLoaded';
 	var $ = {
 		init: function(selector, context) {
 			// domelement && all other queries | modern browsers only!
@@ -8,23 +9,8 @@ $ = function(selector, context) {
 			// if no context is defined, set context to document
 			// use querySelectorAll to get classes, ids etc.
 			// if this is true, return the $ Object else it is a function so return the $.ready(selector)
-			//return !$.isfn(selector) && ($.O = selector.nodeType ? selector : (!context ? document : context).querySelectorAll(selector)) ? $ : $.ready(selector);
-			return !$.isfn(selector) && (selector.nodeType && (($.O = [])[0] = selector) || ($.O = (!context ? document : context).querySelectorAll(selector))) ? $ : $.ready(selector);
-		},
-		ready: function(cb) {
-			// using var for function scope
-			var dcl = 'DOMContentLoaded';
-			$.init(document).bind(dcl, onload = function() {
-				// no need for $.init(document) because the node is already in $.O
-				$.unbind(dcl, onload = null);
-				// using passing $ as first argument to use this as the $ object
-				// second argument to use $ function-like
-				cb.call($, $.init);
-			});
-		},
-		isfn: function(fn) {
-			// not jquery like, but works ;)
-			return typeof fn == 'function';
+			selector.nodeType && (($.O = [])[0] = selector) || ($.O = (!context ? document : context).querySelectorAll(selector));
+			return $;
 		},
 		each: function(cb) {
 			// adapted from jquery && // using var for function scope
@@ -36,16 +22,42 @@ $ = function(selector, context) {
 				this.setAttribute(name, val);
 			});
 		},
-		bind: function(e, cb) {
-			return $.each(function() {
-				(this.a = this.addEventListener) && this.a(e, cb, 0) || (this.a = this.attachEvent) && this.a('on' + e, cb);
-			});
-		},
-		unbind: function(e, cb) {
-			return $.each(function() {
-				(this.r = this.removeEventListener) && this.r(e, cb, 0) || (this.r = this.detachEvent) && this.r('on' + e, cb);
-			});
-		}
+		bind: (function() {
+			if (document.addEventListener) {
+				return function(e, cb) {
+					return $.each(function() {
+						this.addEventListener(e, cb, 0);
+					});
+				};
+			} else {
+				return function(e, cb) {
+					return $.each(function() {
+						this.attachEvent('on' + e, cb);
+					});
+				};
+			}
+		})(),
+		unbind: (function() {
+			if (document.addEventListener) {
+				return function(e, cb) {
+					return $.each(function() {
+						this.removeEventListener(e, cb, 0);
+					});
+				};
+			} else {
+				return function(e, cb) {
+					return $.each(function() {
+						this.detachEvent('on' + e, cb);
+					});
+				};
+			}
+		})()
 	};
-	return $.init(selector, context);
+	$.init(document).bind(dcl, onload = function() {
+		// no need for $.init(document) because the node is already in $.O
+		$.unbind(dcl, onload = null);
+		// using passing $ as first argument to use this as the $ object
+		// second argument to use $ function-like
+		cb.call($, $.init);
+	});
 };
