c7121ce656e207606344ed17406ca1fcbf75f4ba
Florian Harbecke <florian@harbecke.me>
2013-08-08 16:03:57 +0200

    fourth commit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 $ = function(selector, context) {
-	return ($ = {
+	var $ = {
 		init: function(selector, context) {
 			// domelement && all other queries | modern browsers only!
 			// no function so its a string or undefined / null => return $
@@ -8,44 +8,44 @@ $ = function(selector, context) {
 			// if no context is defined, set context to document
 			// use querySelectorAll to get classes, ids etc.
 			// if this is true, return the $ Object else it is a function so return the $.ready(selector)
-			return !$.isfn(selector) && (selector.nodeType && ($.O = []).push(selector) || ($.O = (!context ? document : context).querySelectorAll(selector))) ? $ : $.ready(selector);
+			//return !$.isfn(selector) && ($.O = selector.nodeType ? selector : (!context ? document : context).querySelectorAll(selector)) ? $ : $.ready(selector);
+			return !$.isfn(selector) && (selector.nodeType && (($.O = [])[0] = selector) || ($.O = (!context ? document : context).querySelectorAll(selector))) ? $ : $.ready(selector);
 		},
-		ready: function(fn) {
-			// dual bind the "fire" function
+		ready: function(cb) {
+			// using var for function scope
 			var dcl = 'DOMContentLoaded';
 			$.init(document).bind(dcl, onload = function() {
-				$.init(document).unbind(dcl, onload = null);
-				// using call like jquery
-				fn.call($, $.init);
+				// no need for $.init(document) because the node is already in $.O
+				$.unbind(dcl, onload = null);
+				// using passing $ as first argument to use this as the $ object
+				// second argument to use $ function-like
+				cb.call($, $.init);
 			});
 		},
 		isfn: function(fn) {
 			// not jquery like, but works ;)
 			return typeof fn == 'function';
 		},
-		each: function(fn) {
-			// adapted from jquery
-			for (var i = 0; i < $.O.length; i++) {
-				fn.call(i, $.O[i]);
-			}
+		each: function(cb) {
+			// adapted from jquery && // using var for function scope
+			for (var i = 0; i < $.O.length; i++) cb.call($.O[i], i);
+			return $;
 		},
 		attr: function(name, val) {
-			$.each(function(c) {
-				c.setAttribute(name, val)
+			return $.each(function() {
+				this.setAttribute(name, val);
 			});
-			return $;
 		},
-		bind: function(e, fn) {
-			$.each(function(c) {
-				(c.add = c.addEventListener) && c.add(e, fn, 0);
+		bind: function(e, cb) {
+			return $.each(function() {
+				(this.a = this.addEventListener) && this.a(e, cb, 0) || (this.a = this.attachEvent) && this.a('on' + e, cb);
 			});
-			return $;
 		},
-		unbind: function(e, fn) {
-			$.each(function(c) {
-				(c.rm = c.removeEventListener) && c.rm(e, fn, 0);
+		unbind: function(e, cb) {
+			return $.each(function() {
+				(this.r = this.removeEventListener) && this.r(e, cb, 0) || (this.r = this.detachEvent) && this.r('on' + e, cb);
 			});
-			return $;
 		}
-	}).init(selector, context);
+	};
+	return $.init(selector, context);
 };
